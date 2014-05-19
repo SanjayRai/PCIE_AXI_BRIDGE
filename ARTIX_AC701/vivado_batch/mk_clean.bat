9165ec1037ae2b473c53853c70479de834727214
Sanjay Rai <rai6996@yahoo.com>
2014-05-18 19:09:09 -0700

    Fixed remote HW server script - vivado_debug.tcl

--- ARTIX_AC701/vivado_batch/mk_clean.bat
+++ ARTIX_AC701/vivado_batch/mk_clean.bat
@@ -2,8 +2,10 @@ rm -rf project_X
 rm -rf .Xil 
 rm vivado*.jou 
 rm vivado*.log 
+rm vivado*.png 
 rm fsm_encoding.os
 rm *.ltx 
+rm -rf .hw
 rm -rf .cache
 rm -rf .data
 rm -rf pcie_gen1x1_axi_lite_bridge_example_top.bit
